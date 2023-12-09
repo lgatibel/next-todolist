625cccd8e7d5d29e3f1d70840cc248601011d329
Lionel Gatibelza <lionelgatibelza@gmail.com>
2023-12-09 15:50:26 +0100

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,63 @@
+import './globals.css'
+import type { Metadata, Viewport } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+const APP_NAME = "Todo App";
+const APP_DEFAULT_TITLE = "Todo App";
+const APP_TITLE_TEMPLATE = "%s - Todo App";
+const APP_DESCRIPTION = "Todo Pwa app";
+
+export const metadata: Metadata = {
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  icons: { apple: '/icon-256x256.png'}
+  // openGraph: {
+  //   type: "website",
+  //   siteName: APP_NAME,
+  //   title: {
+  //     default: APP_DEFAULT_TITLE,
+  //     template: APP_TITLE_TEMPLATE,
+  //   },
+  //   description: APP_DESCRIPTION,
+  // },
+  // twitter: {
+  //   card: "summary",
+  //   title: {
+  //     default: APP_DEFAULT_TITLE,
+  //     template: APP_TITLE_TEMPLATE,
+  //   },
+  //   description: APP_DESCRIPTION,
+  // },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
